9ebbe80e7af8aeb421a49aead7ee47e57b3dd8c5
김수환 <suhwan@kakao.com>
2023-12-11 00:00:16 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] dice = new int[3];
+
+        for (int i = 0; i < dice.length; i++) {
+            dice[i] = sc.nextInt();
+        }
+        Arrays.sort(dice);
+
+        if (dice[0] == dice[1] && dice[0] == dice[2]) {
+            // 3개 같음
+            System.out.println(10000 + dice[0] * 1000);
+        } else if (dice[0] == dice[1] && dice[1] != dice[2] || dice[0] != dice[1] && dice[1] == dice[2]) {
+            // 2개 같음
+            System.out.println(1000 + dice[1] * 100);
+        } else {
+            // 모두 다름
+            System.out.println(dice[2] * 100);
+        }
+    }
+}
