87eb6a52f3ade719d287c10ba871bb9d99949984
김수환 <suhwan@kakao.com>
2023-12-23 01:03:00 +0900

    [Silver III] Title: N과 M (4), Time: 284 ms, Memory: 22776 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] arr;
+    public static int n;
+    public static int m;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        arr = new int[m];
+
+        dfs(1, 0);
+
+        System.out.print(sb);
+    }
+
+    public static void dfs(int at, int depth) {
+        if (depth == m) {
+            for (int i : arr) {
+                sb.append(i + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = at; i <= n; i++) {
+            arr[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
