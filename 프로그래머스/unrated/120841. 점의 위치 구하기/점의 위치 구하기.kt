8334c2a6e011ca67cc7b75c68dfa8de01662e24d
김수환 <suhwan@kakao.com>
2023-12-27 01:55:35 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(dot: IntArray): Int {
+        var answer: Int = 0
+
+        if (dot[0] > 0) {
+            if (dot[1] > 0) {
+                answer = 1
+            } else {
+                answer = 4
+            }
+        } else {
+            if (dot[1] > 0) {
+                answer = 2
+            } else {
+                answer = 3
+            }
+        }
+        return answer
+    }
+}
