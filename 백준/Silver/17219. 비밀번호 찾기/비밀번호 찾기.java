cd302191793e73ec170b3129976bc4d359a9ae70
김수환 <suhwan@kakao.com>
2024-01-25 00:16:41 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 2124 ms, Memory: 148336 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        Map<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            map.put(sc.next(), sc.next());
+        }
+
+        for (int i = 0; i < m; i++) {
+            System.out.println(map.get(sc.next()));
+        }
+    }
+}
