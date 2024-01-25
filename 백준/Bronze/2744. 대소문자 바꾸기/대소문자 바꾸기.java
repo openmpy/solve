d02c47ea50dce3f3bbaa7ad6d88fb27441a45473
김수환 <suhwan@kakao.com>
2024-01-26 05:05:34 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 196 ms, Memory: 17480 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -5,15 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String str = sc.next();
-        StringBuilder sb = new StringBuilder();
+        String word = sc.next();
 
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isLowerCase(str.charAt(i))) {
-                sb.append(Character.toUpperCase(str.charAt(i)));
-            } else {
-                sb.append(Character.toLowerCase(str.charAt(i)));
-            }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            sb.append(Character.isLowerCase(ch) ? Character.toUpperCase(ch) : Character.toLowerCase(ch));
         }
         System.out.println(sb);
     }
