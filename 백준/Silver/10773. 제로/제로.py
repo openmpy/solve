d06d5e1c232b5de929f720d5728c387857718597
김수환 <suhwan@kakao.com>
2024-02-06 19:11:12 +0900

    [Silver IV] Title: 제로, Time: 84 ms, Memory: 34024 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,17 @@
+import sys, math
+
+# sys.stdin = open("input.txt", "r")
+
+n = int(sys.stdin.readline().strip())
+
+arr = []
+
+for i in range(n):
+    num = int(sys.stdin.readline().strip())
+
+    if num == 0:
+        arr.pop()
+    else:
+        arr.append(num)
+
+print(sum(arr))
