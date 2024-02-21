3d6819cbdf02660bbfb13ab51d742caade7816c7
김수환 <suhwan@kakao.com>
2024-02-22 06:53:09 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,11 +1,3 @@
-import math
-
 def solution(n):
-    answer = 0
-    
-    if math.sqrt(n).is_integer():
-        answer = (int(math.sqrt(n)) + 1) ** 2
-    else:
-        answer = -1
-        
+    answer = (n ** (1 / 2) + 1) ** 2 if n ** (1 / 2) % 1 == 0 else -1
     return answer
