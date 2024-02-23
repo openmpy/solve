7af3be5128f249b9e35003dcb1ee8b36d62ab9b6
김수환 <suhwan@kakao.com>
2024-02-24 06:17:10 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,13 +1,11 @@
 def solution(array, commands):
     answer = []
     
-    for c in commands:
-        arr = []
+    for cmd in commands:
+        i = cmd[0] - 1
+        j = cmd[1]
         
-        for i in range(c[0] - 1, c[1]):
-            arr.append(array[i])
-            
-        arr = sorted(arr)
-        answer.append(arr[c[2] - 1])
+        arr = sorted(array[i : j])
+        answer.append(arr[cmd[2] - 1])
         
     return answer
