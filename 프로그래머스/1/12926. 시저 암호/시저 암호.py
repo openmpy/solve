db0574528555c4ea497f8be2ac4443d6274b93a0
김수환 <suhwan@kakao.com>
2024-02-24 06:00:32 +0900

    [level 1] Title: 시저 암호, Time: 2.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,16 +1,12 @@
 def solution(s, n):
     answer = ''
     
-    for ch in s:
-        if ch == ' ':
-            answer += ch
-            continue
-        
-        if ch.islower() and ord(ch) + n > ord('z'):
-            answer += chr(ord('a') + (ord(ch) + n) - ord('z') - 1)
-        elif ch.isupper() and ord(ch) + n > ord('Z'): 
-            answer += chr(ord('A') + (ord(ch) + n) - ord('Z') - 1)
+    for c in s:
+        if 'a' <= c <= 'z':
+            answer += chr((ord(c) - ord('a') + n) % 26 + ord('a'))
+        elif 'A' <= c <= 'Z':
+            answer += chr((ord(c) - ord('A') + n) % 26 + ord('A'))
         else:
-            answer += chr(ord(ch) + n)
-        
+            answer += c
+            
     return answer
