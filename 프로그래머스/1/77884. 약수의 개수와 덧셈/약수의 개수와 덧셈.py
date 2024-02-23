c7763a9c42aea8d504bf88d72f09050ac78180b2
김수환 <suhwan@kakao.com>
2024-02-23 13:51:56 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -2,18 +2,6 @@ def solution(left, right):
     answer = 0
     
     for i in range(left, right + 1):
-        if sol(i) % 2 == 0:
-            answer += i
-        else:
-            answer -= i
-            
+        answer += -i if i ** 0.5 % 1 == 0 else i
+        
     return answer
-
-def sol(num):
-    cnt = 0
-    
-    for i in range(1, num + 1):
-        if num % i == 0:
-            cnt += 1
-    
-    return cnt
