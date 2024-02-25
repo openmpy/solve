4d865fc0006642e482a6ca6608e1ec47a7fa7bd9
김수환 <suhwan@kakao.com>
2024-02-25 20:29:30 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,13 +1,10 @@
 def solution(food):
     answer = ''
     
-    for i, n in enumerate(food):
-        if n % 2 == 1:
-            food[i] = n - 1
-            
-    text = ''
     for i in range(1, len(food)):
-        text += str(i) * int(food[i] / 2)
+        if food[i] % 2 == 1:
+            food[i] -= 1
         
-    answer = text + '0' + text[::-1]
-    return answer
+        answer += str(i) * int(food[i] / 2)
+        
+    return answer + '0' + answer[::-1]
