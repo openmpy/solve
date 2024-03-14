177b0b826f9692f095ff383f723df0acf68480aa
김수환 <suhwan@kakao.com>
2024-03-14 09:46:47 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,16 @@
+def solution(ingredient):
+    answer = 0
+    st = []
+    
+    for i in ingredient:
+        st.append(i)
+        
+        if len(st) > 3:
+            if st[-1] == 1 and st[-2] == 3 and st[-3] == 2 and st[-4] == 1:
+                st.pop()
+                st.pop()
+                st.pop()
+                st.pop()
+                answer += 1
+            
+    return answer
