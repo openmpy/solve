8048947aabddb04430831455c011b1089cf53350
김수환 <suhwan@kakao.com>
2024-03-25 09:34:29 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1387.63 ms, Memory: 120 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] elements) {
+        HashSet<Integer> hs = new HashSet<>();
+        
+        for (int i = 1; i <= elements.length; i++) {
+            for (int j = 0; j < elements.length; j++) {
+                int sum = 0;
+                for (int k = j; k < j + i; k++) {
+                    sum += elements[k % elements.length];
+                }
+                hs.add(sum);
+            }
+        }
+		return hs.size();
+    }
+}
